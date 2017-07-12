5a2df513ed23b8e3315a9c82566f1eb384efa008
Vladimir Zahradnik <vladimir.zahradnik@gmail.com>
2017-07-12 22:12:14 +0200

    [ReverseLKM] Limit usage to only one process at a time

--- lesson4_char_driver/reverse/reverse.c
+++ lesson4_char_driver/reverse/reverse.c
@@ -5,6 +5,7 @@
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 #include <linux/cdev.h>
+#include <linux/mutex.h> // Limit LKM use to only one concurrent process
 #define  DEVICE_NAME "reverse"
 #define  CLASS_NAME  "training"
 
@@ -16,9 +17,12 @@ MODULE_VERSION("0.1");
 static int    majorNumber;
 static dev_t  dev_no;
 static char   message[256] = {0};
+static int    numberOpens = 0;
 static struct cdev* kernel_cdev;
 static struct class*  reverseClass  = NULL;
 static struct device* reverseDevice = NULL;
+
+static DEFINE_MUTEX(reverse_mutex);     // Macro to declare a new mutex
 
 // The prototype functions for the character driver -- must come before the struct definition
 static int     dev_open(struct inode *, struct file *);
@@ -36,7 +40,7 @@ static struct file_operations fops =
 
 static int __init reverse_init(void) {
     int ret;
-    printk(KERN_INFO "ReverseLKM: initializing module\n");
+    printk(KERN_INFO "ReverseLKM: Initializing module\n");
 
     // Allocate cdev struct
     kernel_cdev = cdev_alloc();
@@ -45,67 +49,77 @@ static int __init reverse_init(void) {
 
     // Dynamically allocate major device number
     if ((ret = alloc_chrdev_region(&dev_no, 0, 1, DEVICE_NAME)) < 0) {
-        printk(KERN_ALERT "ReverseLKM: failed to register a major number\n");
+        printk(KERN_ALERT "ReverseLKM: Failed to register a major number\n");
         return ret;
     }
 
     majorNumber = MAJOR(dev_no);
-    printk(KERN_INFO "ReverseLKM: registered correctly with major number %d\n", majorNumber);
+    printk(KERN_INFO "ReverseLKM: Registered correctly with major number %d\n", majorNumber);
 
     // Register the device class
     reverseClass = class_create(THIS_MODULE, CLASS_NAME);
     if (IS_ERR(reverseClass)) {
         unregister_chrdev_region(majorNumber, 1);
-        printk(KERN_ALERT "ReverseLKM: failed to register device class\n");
+        printk(KERN_ALERT "ReverseLKM: Failed to register device class\n");
         return PTR_ERR(reverseClass);
     }
-    printk(KERN_INFO "ReverseLKM: device class registered correctly\n");
+    printk(KERN_INFO "ReverseLKM: Device class registered correctly\n");
 
     // Register the device driver -- create node under /dev
     reverseDevice = device_create(reverseClass, NULL, dev_no, NULL, DEVICE_NAME);
     if (IS_ERR(reverseDevice)) {
         unregister_chrdev_region(majorNumber, 1);
-        printk(KERN_ALERT "ReverseLKM: failed to create the device\n");
+        printk(KERN_ALERT "ReverseLKM: Failed to create the device\n");
         return PTR_ERR(reverseDevice);
     }
-    printk(KERN_INFO "ReverseLKM: device node created correctly\n");
+    printk(KERN_INFO "ReverseLKM: Device node created correctly\n");
 
     // Pass cdev struct to kernel. Do it as a last init step, since module
     // should be fully initialized before handling cdev to kernel
     if((ret = cdev_add(kernel_cdev, dev_no, 1)) < 0) {
-        printk(KERN_INFO "Unable to allocate cdev");
+        printk(KERN_INFO "ReverseLKM: Unable to allocate cdev");
         return ret;
     }
 
+    mutex_init(&reverse_mutex);
     return 0;
 }
 
 static void __exit reverse_exit(void) {
+    mutex_destroy(&reverse_mutex);
     cdev_del(kernel_cdev);
     device_destroy(reverseClass, dev_no);
     class_unregister(reverseClass);
     class_destroy(reverseClass);
     unregister_chrdev_region(majorNumber, 1);
-    printk(KERN_INFO "ReverseLKM: module unloaded!\n");
+    printk(KERN_INFO "ReverseLKM: Module unloaded!\n");
 }
 
-static int dev_open(struct inode *inodep, struct file *filep){
+static int dev_open(struct inode *inodep, struct file *filep) {
+    // Try to acquire the mutex (return 0 on fail)
+    if(!mutex_trylock(&reverse_mutex)) {
+        printk(KERN_ALERT "ReverseLKM: Device in use by another process");
+        return -EBUSY;
+    }
+    numberOpens++;
+    printk(KERN_INFO "ReverseLKM: Device has been opened %d time(s)\n", numberOpens);
+    return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
     // TODO: Implement code
     return 0;
 }
 
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
     // TODO: Implement code
     return 0;
 }
 
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-    // TODO: Implement code
-    return 0;
-}
-
-static int dev_release(struct inode *inodep, struct file *filep){
-    // TODO: Implement code
+static int dev_release(struct inode *inodep, struct file *filep) {
+    // Release the mutex (i.e., lock goes up)
+    mutex_unlock(&reverse_mutex);
+    printk(KERN_INFO "ReverseLKM: Device successfully closed\n");
     return 0;
 }
 
